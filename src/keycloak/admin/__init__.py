5490ba7616bdcc56445714948b6c94f1337e74cd
Jason Anderson <jasonanderson@uchicago.edu>
2020-05-19 16:54:20 -0500

    Pass kwargs on admin client calls
    This allows to e.g. add query params on GET calls.

--- src/keycloak/admin/__init__.py
+++ src/keycloak/admin/__init__.py
@@ -60,22 +60,26 @@ class KeycloakAdmin(object):
 
     def post(self, url, data, headers=None, **kwargs):
         return self._realm.client.post(
-            url=url, data=data, headers=self._add_auth_header(headers=headers)
+            url=url, data=data, headers=self._add_auth_header(headers=headers),
+            **kwargs
         )
 
     def put(self, url, data, headers=None, **kwargs):
         return self._realm.client.put(
-            url=url, data=data, headers=self._add_auth_header(headers=headers)
+            url=url, data=data, headers=self._add_auth_header(headers=headers),
+            **kwargs
         )
 
     def get(self, url, headers=None, **kwargs):
         return self._realm.client.get(
-            url=url, headers=self._add_auth_header(headers=headers)
+            url=url, headers=self._add_auth_header(headers=headers),
+            **kwargs
         )
 
     def delete(self, url, headers=None, **kwargs):
         return self._realm.client.delete(
-            url=url, headers=self._add_auth_header(headers=headers), **kwargs
+            url=url, headers=self._add_auth_header(headers=headers),
+            **kwargs
         )
 
     def _add_auth_header(self, headers=None):
